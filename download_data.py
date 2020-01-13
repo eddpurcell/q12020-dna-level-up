ed67b1e3a93ee325d01e8294d81b2418466db11f
Matt Collins <matt.collins2@cox.com>
2020-01-12 20:44:43 -0500

    Adding download helper.

---
+++ download_data.py
@@ -0,0 +1,19 @@
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Download Q1 2020 D&A Level Up Competition Data')
+parser.add_argument('input1', type=str, help="Enter 'preview' for preview of data or 'full' to download all of the data")
+
+args = parser.parse_args()
+
+def main():
+    if args.input1 == 'preview':
+        print('Downloading preview of data...')
+        subprocess.run(['gsutil', 'cp', 'gs://slalom-level-up-q1-2020/data/citibike_stations.csv', '.'])
+
+    if args.input1 == 'full':
+        print('Downloading full data...')
+        subprocess.run(['gsutil', 'cp', 'gs://slalom-level-up-q1-2020/data/*.csv', '.'])
+
+if __name__ == '__main__':
+    main()
